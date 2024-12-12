9be733b2471fb388f869f105ed39faa84aac72a3
adfinem <zenlaws02@gmail.com>
2024-12-13 02:00:37 +0800

    feat: in progress, working on app directory

---
+++ app/index.tsx
@@ -0,0 +1,27 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
