05d9c4c1de72917646883af427123384f0410e71
adfinem <zenlaws02@gmail.com>
2024-12-15 23:15:35 +0800

    feat: adding simple navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Index() {
   return (
@@ -9,7 +10,10 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Home</Text>
+      <Link href={'/about'} style={styles.link}>
+        Go to About
+      </Link>
     </View>
   );
 }
@@ -23,5 +27,8 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+  },
+  link: {
+    textDecorationLine: 'underline',
   },
 });
