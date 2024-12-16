7bf2ece6f4e76b66aa25b09cfb677a620805a8b4
adfinem <zenlaws02@gmail.com>
2024-12-17 02:57:52 +0800

    feat: in progress, added Button component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,19 @@
 import { View, StyleSheet } from 'react-native';
-import ImageViewer from '../../components/ImageViewer';
-import Button from '../../components/Button';
 
-const PlaceholderImage = require('../../assets/images/background-image.png');
+import Button from '@/components/Button';
+import ImageViewer from '@/components/ImageViewer';
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} />
-        <View style={styles.footerContainer}>
-          <Button label="Choose a photo" />
-          <Button label="Use this photo" />
-        </View>
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -23,13 +24,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   imageContainer: {
     flex: 1,
+    paddingTop: 28,
   },
   footerContainer: {
-    flex: 1 / 3,
+    flex: 1 / 2,
     alignItems: 'center',
   },
 });
