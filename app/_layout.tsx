6d84fe2013428c1d8d04afee2520bb67b67ae4a9
adfinem <zenlaws02@gmail.com>
2024-12-19 15:39:41 +0800

    feat: added gesture to increase sticker size

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Fragment } from 'react';
 import { StyleSheet, LogBox } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
-    <Fragment>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <Stack>
         <Stack.Screen
@@ -17,7 +18,7 @@ export default function RootLayout() {
           }}
         />
       </Stack>
-    </Fragment>
+    </GestureHandlerRootView>
   );
 }
 
